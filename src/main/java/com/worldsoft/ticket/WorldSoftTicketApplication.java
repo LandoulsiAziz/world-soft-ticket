0af1608e3fa21ef64edccbe67f00632f0917694e
LANDOULSI Mohamed Aziz <medaziz.landolsi@etudiant-isi.utm.tn>
2021-08-27 15:56:30 +0200

    Initial project commit

---
+++ src/main/java/com/worldsoft/ticket/WorldSoftTicketApplication.java
@@ -0,0 +1,13 @@
+package com.worldsoft.ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldSoftTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorldSoftTicketApplication.class, args);
+    }
+
+}
